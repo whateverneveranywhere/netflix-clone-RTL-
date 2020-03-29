f1bcc64931e8d38847c5e66abadb68fcfb2dbba4
Sean Walker <firstexhotic@gmail.com>
2020-03-29 19:50:28 +0430

    test

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-navigator.serviceWorker.register('sw.js')
-.then(function(swReg) {
-  console.log('Service Worker is registered', swReg);
-
-  swRegistration = swReg;
-  initializeUI();
-})
--- sw.js
+++ sw.js
@@ -1,32 +1,34 @@
-// TODO: increase `version` number to force cache update when publishing a new release
-const version = 'v2';
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = [
+    '/offline.html',
+  ];
 
-const config = {
-    cacheRemote: true,
-    version: version+'::',
-    preCachingItems: [
-        'app.bundle.js',
-        'index.html',
-        'offline.html',
-        '404.html',
-        'sw.js'
-    ],
-    blacklistCacheItems: [
-        'index.html',
-        'service-worker.js'
-    ],
-    offlineImage: '<svg role="img" aria-labelledby="offline-title"' + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">' + '<title id="offline-title">Offline</title>' + '<g fill="none" fill-rule="evenodd"><path fill="#121212" d="M0 0h400v300H0z"/>' + '<text fill="#aaa" font-family="monospace" font-size="32" font-weight="bold">' + '<tspan x="136" y="156">offline</tspan></text></g></svg>',
-    offlinePage: 'offline.html',
-    notFoundPage: '404.html',
-    offlineTmdbResults: {
-        page: 1,
-        results: [{
-            "title": "Offline",
-            "poster_path": "https://tmdb.org/offline.jpg",
-            "backdrop_path": "https://tmdb.org/offline.jpg",
-            "overview": ""
-        }],
-        total_pages: 1,
-        total_results: 1
-    }
-};
+
+
+// CODELAB: Remove previous cached data from disk.
+evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+);
+
+
+// CODELAB: Add fetch event handler here.
+if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+      fetch(evt.request)
+          .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                  return cache.match('offline.html');
+                });
+          })
+  );
