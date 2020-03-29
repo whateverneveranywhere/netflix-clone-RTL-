1aef3287e7250680caefd0405c84a09d42511446
Sean Walker <firstexhotic@gmail.com>
2020-03-29 19:27:09 +0430

    installable

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,7 @@
-if ('serviceWorker' in navigator) {  
-    navigator.serviceWorker.register('/sw.js')
-    .then(function (registration) { 
-    console.log('ServiceWorker registration succeeded.');       
-    }).catch(function (err) { 
-     console.log('ServiceWorker registration failed: ', err);  
-    });
-   }
+navigator.serviceWorker.register('sw.js')
+.then(function(swReg) {
+  console.log('Service Worker is registered', swReg);
+
+  swRegistration = swReg;
+  initializeUI();
+})
