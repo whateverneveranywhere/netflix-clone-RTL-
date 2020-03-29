60729fcb24a4577917e71ef8b09fa3cc49979384
Sean Walker <firstexhotic@gmail.com>
2020-03-29 20:22:17 +0430

    test

--- sw.js
+++ sw.js
@@ -1,34 +1,25 @@
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
-    '/offline.html',
-  ];
 
-
-
-// CODELAB: Remove previous cached data from disk.
-evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-);
-
-
-// CODELAB: Add fetch event handler here.
-if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
-  }
-  evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
-          })
+const CACHE_NAME = "cache-v1";
+const assetToCache = [
+  "/offline.html"
+];
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(assetToCache);
+      })
+      .catch(console.error)
   );
+});
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response; 
+      }
+      return fetch(event.request);
+    })
+  );
+});
